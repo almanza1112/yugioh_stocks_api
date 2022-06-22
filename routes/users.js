0fdff6d5994d5942f6fc97203bcd2979fab3bbb0
almanza1112 <almanza1112@gmail.com>
2022-06-22 03:30:44 -0400

    tutorial done

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,8 @@ router.get('/', async (req, res) => {
 })
 
 // Getting one
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/:id', getUser, (req, res) => {
+    res.send(res.user)
 })
 
 // Creating one
@@ -34,13 +34,44 @@ router.post('/', async (req, res) => {
 })
 
 // Updating one
-router.patch('/:id', (req, res) => {
-
+router.patch('/:id', getUser, async (req, res) => {
+    if (req.body.name != null ){
+        res.user.name = req.body.name
+    }
+    if (req.body.isSubscribed != null ){
+        res.user.isSubscribed = req.body.isSubscribed
+    }
+    try {
+        const updatedUser = await res.user.save()
+        res.json(updatedUser)
+    } catch (err) {
+        res.status(400).json({message:err.message})
+    }
 })
 
 // Deleting one
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', getUser, async (req, res) => {
+    try {
+        await res.user.remove()
+        res.json({message: 'Deleted user'})
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
 })
+
+async function getUser(req, res, next) {
+    let user
+    try {
+        user = await User.findById(req.params.id)
+        if (user == null){
+            return res.status(400).json({message: 'Cannot find user'})
+        }
+    } catch (err) {
+        return res.status(500).json({message: err.message})
+    }
+
+    res.user = user
+    next()
+}
 
 module.exports = router

4c9df2dcb4f9e204df8820ea85a461ef8f7ec2ea
almanza1112 <almanza1112@gmail.com>
2022-07-01 05:16:56 -0400

    i finally handled return json

--- routes/cards.js
+++ routes/cards.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const Card = require('../models/card')
 const fetch = require('node-fetch')
 const EbayAuthToken = require('ebay-oauth-nodejs-client')
+const zlib = require('zlib');
+
 
 
 const ebayAuthToken = new EbayAuthToken({
@@ -12,34 +14,36 @@ const ebayAuthToken = new EbayAuthToken({
 //GET CARDS
 router.get('/', async (req, res) => {
     (async () => {
-        
         const token = await ebayAuthToken.getApplicationToken('SANDBOX', 'https://api.ebay.com/oauth/api_scope');
         const parsedToken = JSON.parse(token)
         const url = 'https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q=drone&limit=3'
     
-     const respizzle = await fetch(url ,{
-        method: 'GET',
-        headers:{
-            'Authorization':'Bearer ' + parsedToken.access_token,
-            'Content-Type':'application/json',
-            'X-EBAY-C-MARKETPLACE-ID':'EBAY_US',
-            'X-EBAY-C-ENDUSERCTX':'affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>'
-        }
-        })
-        .then(res => {
-            if (res.ok){
-                console.log(res.headers)
-            } else {
-                console.log("i suck: " + res.status)
+        const respizzle = await fetch(url ,{
+            method: 'GET',
+            headers:{
+                'Authorization':'Bearer ' + parsedToken.access_token,
+                'Content-Type':'application/json',
+                'X-EBAY-C-MARKETPLACE-ID':'EBAY_US',
+                'X-EBAY-C-ENDUSERCTX':'affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>',
+                'encoding': null
             }
-        })
-        .catch(err => {
-            console.error({'message': err.message})
-        })
-    
-    res.send(respizzle)
+            })
+            /*
+            .then(res => {
+                if (res.ok){
+                              
+                } else {
+                    console.log("i suck: " + res.status)
+                }
+            })
+            .catch(err => {
+                console.error({'FETCH MESSAGE': err.message})
+            })*/
 
-
+        const data  = await respizzle.json()
+        console.log(data)
+        
+        res.send(respizzle)
     })();   
 })
 

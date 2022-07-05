70e87c7efcd945d38ae6581e2f04555001c815ca
almanza1112 <almanza1112@gmail.com>
2022-07-05 13:58:34 -0400

    updated card schema to populate database

--- models/card.js
+++ models/card.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose')
 
 const cardSchema = new mongoose.Schema({
-    name:{
-        type: String, 
-        required: true
-    },
-    set:{
-        type: String,
-        required: true
-    },
-    rarity:{
-        type: String, 
-        required: true
+    card_id: {type: String, required: true},
+    name: { type: String, required: true },
+    type: { type: String, required: false },
+    description: { type: String, required: false },
+    race: { type: String, required: false },
+    archetype: { type: String, required: false },
+    card_sets:{
+        set_name: { type: String, required: true },
+        set_code: { type: String, required: true },
+        set_rarity: { type: String, required: true } },
+    card_images: {
+        image_url: { type: String, required: false },
+        image_url_small: {type: String, required: false }
     }
 })
 
